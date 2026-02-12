faec84c4f41d14764b838da4b84cee3ac8cee612
Lychee304 <2173013@talnet.nl>
2026-02-12 11:41:35 +0100

    player faces correctly
    the player now turns in the same direction as its travelling, its eyes dissappears, but i know what the issue is

--- Waterstone/Assets/Scripts/PlayerInput.cs
+++ Waterstone/Assets/Scripts/PlayerInput.cs
@@ -36,6 +36,7 @@ public class PlayerInput : MonoBehaviour
             print("ahoy");
             Vector3 position = new Vector3(0, 0, 0);
             transform.position += (new Vector3(-_speed, 0, 0) * Time.deltaTime * _speed);
+            transform.rotation = Quaternion.Euler(0, 180, 0);
         }
 
         if (Input.GetKey(KeyCode.S))
@@ -50,6 +51,7 @@ public class PlayerInput : MonoBehaviour
             print("dag");
             Vector3 position = new Vector3(0, 0, 0);
             transform.position += (new Vector3(_speed, 0, 0) * Time.deltaTime * _speed);
+            transform.rotation = Quaternion.Euler(0, 0, 0);
         }
     }
     /*
