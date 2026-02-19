7a529317714cd826fcbd36286ade74b7727adc15
Lychee304 <2173013@talnet.nl>
2026-02-19 11:57:49 +0100

    added all the code
    doesnt work yet, but very close!

--- Waterstone/Assets/Scripts/EnemyDamage.cs
+++ Waterstone/Assets/Scripts/EnemyDamage.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
 
-public class EnemyDamage
+public class EnemyDamage : MonoBehaviour
 {
+    public PlayerHealth playerHealth;
+    public int damage = 20;
+    void Start()
+    {
+
+    }
     
+    void Update()
+    {
+
+    }
+
+    // void da warrenty haa gottem
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            playerHealth.TakeDamage(damage);
+        }
+    }
 }
